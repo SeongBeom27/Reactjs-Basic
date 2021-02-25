fcc07af1292fda078c6a7b80c02a3312d658da64
SeongBeom27 <lgh121546@naver.com>
2021-02-25 17:08:32 +0900

    React JS 4. Component 개념과 생성

---
+++ src/Potato.js
@@ -0,0 +1,10 @@
+import React from "react";          // Import 해주어야 React가 여기에 jsx가 있는 component를 인식
+
+// 함수의 맨 앞이 대문자이어야 외부에서 사용할 수 있다.
+function Potato(){
+    return (
+        <div><h3>I love Potato</h3></div>
+    );
+}
+
+export default Potato;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-
+import Potato from './Potato';
+/**
+ * Component
+ * 
+ * <App /> : 이 부분을 react에서 component라고 부른다
+ * 
+ *      동작 : HTML을 반환하는 함수
+ *      -> 리액트는 컴포넌트로 이루어져 있다. ( 컴포넌트로 HTML 코드를 표시한다. ) 
+ * 
+ *      Javascript와 HTML 사이 이러한 조합을 jsx라고 부른다. ( In React )
+ *  */ 
 ReactDOM.render( <App />, document.getElementById('potato'));
 
